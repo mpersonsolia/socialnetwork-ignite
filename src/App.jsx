989b91918b9fcbf41dca243ce5848bf3f3cb02b3
Maria Paula Person Solia <79114619+mpersonsolia@users.noreply.github.com>
2022-08-03 14:14:09 -0300

    add responsive css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,64 @@ import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 import './global.css';
 
+//posts para simular o back end
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: 'https://github.com/mpersonsolia.png',
+			name: 'Maria Paula',
+			role: 'Web Developer @ Allìms',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galeraa 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: 'jane.design/doctorcare' },
+		],
+		publishedAt: new Date('2022-08-03 20:58:12'),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: 'https://github.com/Rocketseat.png',
+			name: 'Rocketseat',
+			role: 'CEO @ Rocketseat',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galeraa 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: 'jane.design/doctorcare' },
+		],
+		publishedAt: new Date('2022-08-13 02:08:10'),
+	},
+	{
+		id: 3,
+		author: {
+			avatarUrl: 'https://github.com/developerAllims.png',
+			name: 'Allìms',
+			role: 'CEO @ Allìms',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galeraa 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: 'jane.design/doctorcare' },
+		],
+		publishedAt: new Date('2022-08-18 20:01:12'),
+	},
+];
+
 function App() {
 	return (
 		<div>
@@ -11,8 +69,15 @@ function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post />
-					<Post />
+					{posts.map((post) => {
+						return (
+							<Post
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						);
+					})}
 				</main>
 			</div>
 		</div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,15 +2,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author }) {
 	return (
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<Avatar src='https://github.com/developerAllims.png' />
+					<Avatar src={author.avatarUrl} />
 					<div className={styles.authorInfo}>
-						<strong>Allìms</strong>
-						<span>Full Stack Developer</span>
+						<strong>{author.name}</strong>
+						<span>{author.role}</span>
 					</div>
 				</div>
 
@@ -19,20 +19,7 @@ export function Post() {
 				</time>
 			</header>
 
-			<div className={styles.content}>
-				<p>Fala galeraa 👋</p>
-				<p>
-					Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-					no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-				</p>
-				<p>
-					👉<a href=''>jane.design/doctorcare</a>
-				</p>
-				<p>
-					<a href=''>#novoprojeto</a> <a href=''>#nlw</a>{' '}
-					<a href=''>#rocketseat</a>
-				</p>
-			</div>
+			<div className={styles.content}></div>
 
 			<form className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
